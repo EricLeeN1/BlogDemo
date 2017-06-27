ff1b1d0b7331c5ba154ca56fc9e7bf350d2e8962
EricLeeN1 <179310009@qq.com>
2017-06-27 15:29:40 +0800

    001

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // 应用程序的启动(入口)文件
 //加载express模块
-var express = require('express');
+const express = require('express');
 //创建app应用=>NodeJS http.createserver();
-var app = express();
+const app = express();
+
+/**
+ * 首页
+ *    req request对象
+ *    res response对象
+ *    next 函数
+ */
+app.get('/', (req, res, next) => {
+    res.send('<h1>欢迎光临我的博客</h1>');
+})
+
 
 //监听http请求
 app.listen(8081);
