28c0600a17942ded69b6450b7db82c97ddd69ae1
EricLee <179310009@qq.com>
2017-07-01 22:20:09 +0800

    001

--- routers/admin.js
+++ routers/admin.js
@@ -60,16 +60,40 @@ router.get('/user', function(req, res) {
                 page: page
             });
         });
-    })
+    });
 });
 /**
  * 分类首页
  * 
  */
 router.get('/category', function(req, res) {
-    res.render('admin/category_index', {
-        userInfo: req.userInfo
-    });
+    var page = Number(req.query.page || 1);
+    var limit = 4;
+
+    var pages = 0;
+    Category.count().then(function(count) {
+        // 计算总页数
+        pages = Math.ceil(count / limit);
+        //取值不能超过pages
+        page = Math.min(page, pages);
+        //取值不能小于1
+        page = Math.max(page, 1);
+
+        var skip = (page - 1) * limit;
+
+        Category.find().limit(limit).skip(skip).then(function(categories) {
+            // console.log(users);
+            res.render('admin/category_index', {
+                userInfo: req.userInfo,
+                categories: categories,
+
+                count: count,
+                pages: pages,
+                limit: limit,
+                page: page
+            });
+        });
+    })
 });
 /**
  * 添加分类
@@ -116,6 +140,114 @@ router.post('/category/add', function(req, res) {
             url: '/admin/category'
         });
     });
-})
-
+});
+/*
+* 分类修改
+* */
+router.get('/category/edit',function (req,res) {
+   //获取要修改的分类的信息，并且用表单的形式展现出来
+    var id = req.query.id || '';
+    //获取要修改的分类信息
+    Category.findOne({
+        _id:id
+    }).then(function (category) {
+        if (!category) {
+            res.render('admin/error',{
+                userInfo:req.userInfo,
+                msg:'分类信息不存在'
+            });
+        }else {
+            res.render('admin/category_edit',{
+                userInfo:req.userInfo,
+                category:category
+            });
+        }
+    });
+});
+/*
+* 分类修改的保存
+* */
+router.post('/category/edit', function(req, res) {
+    //获取要修改的分类的信息，并且用表单的形式展现出来
+    var id = req.query.id || '';
+    //获取post提交的名字
+    var name = req.body.name || '';
+    Category.findOne({
+        _id: id
+    }).then(function(category) {
+        if (!category) {
+            //数据库中不存在该分类
+            res.render('admin/error', {
+                userInfo: req.userInfo,
+                msg: '分类信息不存在'
+            });
+            return Promise.reject();
+        } else {
+            //当用户没有做任何的修改提交的时候
+            if (name == category.name) {
+                res.render('admin/success', {
+                    userInfo: req.userInfo,
+                    msg: '修改成功',
+                    url:'/admin/category'
+                });
+                return Promise.reject();
+            }else {
+                return Category.findOne({
+                    _id:{$ne:id},
+                    name:name
+                });
+            }
+        }
+    }).then(function(sameCategory) {
+        if (sameCategory) {
+            //数据库中已经存在该分类了
+            res.render('admin/error', {
+                userInfo: req.userInfo,
+                msg: '数据库中已经存在同名分类'
+            });
+            return Promise.reject();
+        }else{
+            return Category.update({
+                _id:id
+            },{
+                name:name
+            });
+        }
+    }).then(function () {
+        res.render('admin/success', {
+            userInfo: req.userInfo,
+            msg: '修改成功',
+            url:'/admin/category'
+        });
+    });
+});
+/*
+* 分类删除
+* */
+router.get('/category/delete',function (req,res) {
+    //获取要删除的分类的信息，并且用表单的形式展现出来
+    var id = req.query.id || '';
+    Category.findOne({
+        _id: id
+    }).then(function (category) {
+        if (!category) {
+            //数据库中不存在该分类
+            res.render('admin/error', {
+                userInfo: req.userInfo,
+                msg: '分类信息不存在'
+            });
+            return Promise.reject();
+        }else {
+            Category.remove({
+                _id:id
+            }).then(function () {
+                res.render('admin/success', {
+                    userInfo: req.userInfo,
+                    msg: '删除成功',
+                    url:'/admin/category'
+                });
+            });
+        }
+    });
+});
 module.exports = router;
